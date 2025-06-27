959e9355e55b3de88eccf351c2b3990a6c52c43b
johnathost <contact@yannisvoyatzis.com>
2025-06-27 04:50:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,6 +35,17 @@ sed -i_orig -e 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
 # disable bitmap compression since its local its much faster
 sed -i_orig -e 's/bitmap_compression=true/bitmap_compression=false/g' /etc/xrdp/xrdp.ini
 
+# Add script to setup the debian session properly
+if [ ! -e /etc/xrdp/startdebian.sh ]; then
+cat >> /etc/xrdp/startdebian.sh << EOF
+#!/bin/sh
+export GNOME_SHELL_SESSION_MODE=debian
+export XDG_CURRENT_DESKTOP=debian:GNOME
+exec /etc/xrdp/startwm.sh
+EOF
+chmod a+x /etc/xrdp/startdebian.sh
+fi
+
 # use the script to setup the debian session
 sed -i_orig -e 's/startwm/startdebian/g' /etc/xrdp/sesman.ini
 
@@ -44,27 +55,10 @@ sed -i -e 's/FuseMountName=thinclient_drives/FuseMountName=shared-drives/g' /etc
 # Changed the allowed_users
 sed -i_orig -e 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
 
-# Blacklist the vmw module
-if [ ! -e /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf ]; then
-cat >> /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf <<EOF
-blacklist vmw_vsock_vmci_transport
-EOF
-fi
-
 #Ensure hv_sock gets loaded
 if [ ! -e /etc/modules-load.d/hv_sock.conf ]; then
 echo "hv_sock" > /etc/modules-load.d/hv_sock.conf
 fi
-
-# Configure the policy xrdp session
-cat > /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla <<EOF
-[Allow Colord all Users]
-Identity=unix-user:*
-Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
-ResultAny=no
-ResultInactive=no
-ResultActive=yes
-EOF
 
 # reconfigure the service
 systemctl daemon-reload
