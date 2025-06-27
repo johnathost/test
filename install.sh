e74d184edcc9c41c72ab162ba304e7f0090f28f6
johnathost <contact@yannisvoyatzis.com>
2025-06-27 04:44:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,124 +15,103 @@ fi
 echo "Updating package list..."
 apt update
 
+###############################################################################
+# XRDP
+#
+
 # Install required packages for Hyper-V Enhanced Session Mode
 echo "Installing hyperv-daemons and xrdp..."
-apt install -y hyperv-daemons xrdp xorgxrdp
+apt install -y hyperv-daemons xrdp
 
-# Enable and start xrdp service
-echo "Enabling and starting xrdp service..."
-systemctl enable xrdp
+# Configure the installed XRDP ini files.
+# use vsock transport.
+sed -i_orig -e 's/use_vsock=true/use_vsock=false/g' /etc/xrdp/xrdp.ini
+# change the port
+sed -i_orig -e 's@port=3389@port=vsock://-1:3389@g' /etc/xrdp/xrdp.ini
+# use rdp security.
+sed -i_orig -e 's/security_layer=negotiate/security_layer=rdp/g' /etc/xrdp/xrdp.ini
+# remove encryption validation.
+sed -i_orig -e 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
+# disable bitmap compression since its local its much faster
+sed -i_orig -e 's/bitmap_compression=true/bitmap_compression=false/g' /etc/xrdp/xrdp.ini
+
+# Add script to setup the debian session properly
+if [ ! -e /etc/xrdp/startdebian.sh ]; then
+cat >> /etc/xrdp/startdebian.sh << EOF
+#!/bin/sh
+export GNOME_SHELL_SESSION_MODE=debian
+export XDG_CURRENT_DESKTOP=debian:GNOME
+exec /etc/xrdp/startwm.sh
+EOF
+chmod a+x /etc/xrdp/startdebian.sh
+fi
+
+# use the script to setup the debian session
+sed -i_orig -e 's/startwm/startdebian/g' /etc/xrdp/sesman.ini
+
+# rename the redirected drives to 'shared-drives'
+sed -i -e 's/FuseMountName=thinclient_drives/FuseMountName=shared-drives/g' /etc/xrdp/sesman.ini
+
+# Changed the allowed_users
+sed -i_orig -e 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
+
+# Blacklist the vmw module
+if [ ! -e /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf ]; then
+cat >> /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf <<EOF
+blacklist vmw_vsock_vmci_transport
+EOF
+fi
+
+#Ensure hv_sock gets loaded
+if [ ! -e /etc/modules-load.d/hv_sock.conf ]; then
+echo "hv_sock" > /etc/modules-load.d/hv_sock.conf
+fi
+
+# Configure the policy xrdp session
+cat > /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla <<EOF
+[Allow Colord all Users]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EOF
+
+# reconfigure the service
+systemctl daemon-reload
+sudo systemctl enable xrdp.service
+sudo systemctl enable xrdp-sesman.service
 systemctl start xrdp
 
-# Configure xrdp to use Enhanced Session Mode
-echo "Configuring xrdp for Enhanced Session Mode..."
-cat << EOF > /etc/xrdp/xrdp.ini
-[Globals]
-ini_version=1
-fork=true
-port=3389
-use_vsock=false
-tcp_nodelay=true
-security_layer=rdp
-crypt_level=high
-certificate=
-key_file=
-ssl_protocols=TLSv1.2, TLSv1.3
+#
+# End XRDP
+###############################################################################
 
-[xrdp1]
-name=sesman-X11rdp
-lib=libxrdp.so
-username=ask
-password=ask
-ip=127.0.0.1
-port=-1
-code=20
-EOF
+###############################################################################
+# Pulseaudio
+#
 
-# Restart xrdp to apply changes
-systemctl restart xrdp
+pulsever=$(pulseaudio --version | awk '{print $2}')
 
-# Install PulseAudio for sound redirection
-echo "Installing PulseAudio for sound redirection..."
-apt install -y pulseaudio pulseaudio-module-xrdp
+sudo apt build-dep pulseaudio -y
 
-# Load PulseAudio xrdp modules
-echo "Configuring PulseAudio for xrdp..."
-cat << EOF > /etc/xrdp/pulseaudio.ini
-[pulseaudio]
-sink_name=xrdp-sink
-source_name=xrdp-source
-EOF
+cd /tmp
+sudo apt source pulseaudio=$pulsever
+cd /tmp/pulseaudio-$pulsever
+sudo ./configure
 
-# Ensure PulseAudio modules are loaded for xrdp
-echo "load-module module-xrdp-sink" >> /etc/pulse/default.pa
-echo "load-module module-xrdp-source" >> /etc/pulse/default.pa
+git clone https://github.com/neutrinolabs/pulseaudio-module-xrdp.git
+cd pulseaudio-module-xrdp
+sudo ./bootstrap
+sudo ./configure PULSE_DIR="/tmp/pulseaudio-$pulsever"
+sudo make
 
-# Set permissions for PulseAudio
-echo "Setting PulseAudio permissions..."
-adduser xrdp pulse-access
-adduser xrdp audio
+cd /tmp/pulseaudio-$pulsever/pulseaudio-module-xrdp/src/.libs
+sudo install -t "/var/lib/xrdp-pulseaudio-installer" -D -m 644 *.so
+sudo install -t "/usr/lib/pulse-$pulsever/modules" -D -m 644 *.so
 
-# Enable and start PulseAudio (system-wide for xrdp)
-echo "Starting PulseAudio..."
-systemctl --user enable pulseaudio
-systemctl --user start pulseaudio
+#
+# End Pulseaudio
+###############################################################################
 
-# Configure Hyper-V integration services
-echo "Ensuring Hyper-V integration services are enabled..."
-cat << EOF > /etc/modules-load.d/hyperv.conf
-hv_vmbus
-hv_storvsc
-hv_blkvsc
-hv_netvsc
-hv_utils
-hv_balloon
-EOF
-
-# Update initramfs to include Hyper-V modules
-echo "Updating initramfs..."
-update-initramfs -u
-
-# Create a script to ensure services start correctly
-echo "Creating startup script for Hyper-V services..."
-cat << EOF > /usr/local/bin/hyperv-enhanced.sh
-#!/bin/bash
-modprobe hv_vmbus
-modprobe hv_storvsc
-modprobe hv_blkvsc
-modprobe hv_netvsc
-modprobe hv_utils
-modprobe hv_balloon
-systemctl restart xrdp
-systemctl --user restart pulseaudio
-EOF
-
-chmod +x /usr/local/bin/hyperv-enhanced.sh
-
-# Add to systemd to run at boot
-echo "Setting up systemd service for Hyper-V enhancements..."
-cat << EOF > /etc/systemd/system/hyperv-enhanced.service
-[Unit]
-Description=Hyper-V Enhanced Session and Audio Setup
-After=network.target xrdp.service
-
-[Service]
-Type=oneshot
-ExecStart=/usr/local/bin/hyperv-enhanced.sh
-RemainAfterExit=yes
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Enable the custom service
-systemctl enable hyperv-enhanced.service
-
-# Final instructions
-echo "Setup complete!"
-echo "Please reboot the VM to apply all changes."
-echo "After reboot, connect using Hyper-V Enhanced Session Mode."
-echo "Ensure the Hyper-V host has 'Enhanced Session Mode' enabled in VM settings."
-echo "For audio, verify that 'Audio redirection' is enabled in the RDP client."
-
-exit 0
+echo "Reboot your machine to begin using XRDP."
